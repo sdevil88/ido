dd5017336824ef6a7571405f42f1a6b04ce7f104
sagar <wwwghimiresagar88@gmail.com>
2023-08-06 12:52:36 +0545

    login form validation

---
+++ index3.js
@@ -0,0 +1,27 @@
+function valid(){
+  const details = {
+    name: "",
+    password: "",
+    dreturn: function()
+    {
+        details.name = document.getElementById("name").value ;
+        details.password = document.getElementById("password").value ;
+        return this.uname+this.password;
+    },
+    check: function()
+    {
+        if(details.name==="sagar" && details.password==="ghimire")
+        {
+            alert("login ");
+            window.location="index.html";
+        }else{
+            alert("login failed ");
+            window.location="home.html";
+        }
+    }
+
+  }
+    details.dreturn();
+    details.check();
+
+}
