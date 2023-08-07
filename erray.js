863d3dd3fcd30254171cf6ee43378bd2ac0d451f
sagar <wwwghimiresagar88@gmail.com>
2023-08-07 08:58:05 +0545

    erray

---
+++ erray.js
@@ -0,0 +1,24 @@
+//  let a = ["sagar","ghimire","a",3,4,"a","c","b"];
+// b = a.length;//check erray length 
+// //c = a.pop();//pop last value from erray 
+
+// //console.log(a,b,c)//display 
+// d = a.push(5);
+// console.log(a,d);//
+
+
+// b = a.sort();//short wise= ascending order;
+// console.log(b)
+
+// let x;
+// x=a.toString(" or");//join all as string
+// console.log(a,x,typeof x);
+// let b =a.toString();//join all as string
+// console.log(b);
+
+
+
+// let b = a[1];//erray value path
+// console.log(b)
+
+
